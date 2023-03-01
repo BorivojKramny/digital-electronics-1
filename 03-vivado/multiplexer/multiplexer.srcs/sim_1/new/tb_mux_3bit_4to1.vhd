1762fb571099d1e7f7d8cc5131732adbbf7a8f57
BorivojKramny <124801292+BorivojKramny@users.noreply.github.com>
2023-03-01 11:45:18 +0100

    Update tb_mux_3bit_4to1.vhd

--- 03-vivado/multiplexer/multiplexer.srcs/sim_1/new/tb_mux_3bit_4to1.vhd
+++ 03-vivado/multiplexer/multiplexer.srcs/sim_1/new/tb_mux_3bit_4to1.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 22.02.2023 16:25:49
+-- Create Date: 02/22/2023 04:36:21 PM
 -- Design Name: 
 -- Module Name: tb_mux_3bit_4to1 - Behavioral
 -- Project Name: 
@@ -35,9 +35,53 @@ entity tb_mux_3bit_4to1 is
 --  Port ( );
 end tb_mux_3bit_4to1;
 
-architecture Behavioral of tb_mux_3bit_4to1 is
+architecture testbench of tb_mux_3bit_4to1 is
 
+  -- Testbench local signals
+  signal sig_sel           : std_logic_vector(1 downto 0);
+  signal sig_a             : std_logic_vector(2 downto 0);
+  signal sig_b             : std_logic_vector(2 downto 0);
+  signal sig_c             : std_logic_vector(2 downto 0);
+  signal sig_d             : std_logic_vector(2 downto 0);
+  signal sig_out             : std_logic_vector(2 downto 0);
+  
 begin
+uut_mux_3bit_4to1 : entity work.mux_3bit_4to1
+port map (
+      a_i           => sig_a,
+      b_i           => sig_b,
+      c_i           => sig_c,
+      d_i           => sig_d,
+      f_o           => sig_out,
+      sel           => sig_sel
+    );
+ p_stimulus : process is
+  begin
 
+    -- Report a note at the beginning of stimulus process
+    report "Stimulus process started";
 
-end Behavioral;
+    -- First test case ...
+    sig_b <= "000";
+    sig_a <= "001";
+    sig_c <= "011";
+    sig_d <= "111";
+    wait for 100 ns;
+    
+    sig_sel <= "00";
+    wait for 100 ns;
+    sig_sel <= "01";
+    wait for 100 ns;
+    sig_sel <= "10";
+    wait for 100 ns;
+    sig_sel <= "11";
+    wait for 100 ns;
+    
+    -- Report a note at the end of stimulus process
+    report "Stimulus process finished";
+
+    wait; -- Data generation process is suspended forever
+
+  end process p_stimulus;
+
+end architecture testbench;
